dfbe895ac944355ff2e2623ecc73035e6676a59d
Rodrigo Chávez <rodalcham@gmail.com>
2024-11-17 10:57:35 +0100

    added pngs

--- app/screens/CityMap.js
+++ app/screens/CityMap.js
@@ -48,7 +48,34 @@ export const CityMap = () => {
                     title="Your Location"
                     description="This is where you are!"
                 />
-				<Grid rows={40} columns={35} latstart={49.125} lontstart={9.203}/>
+				<Marker
+                    coordinate={{
+                        latitude: 49.152,
+                        longitude: 9.216,
+                    }}
+                    title="Bus Line 42"
+                    description="This icon follows bus line 42"
+                    image={require('../../assets/bus.png')} // Replace with your local image path
+                />
+				<Marker
+                    coordinate={{
+                        latitude: 49.16,
+                        longitude: 9.208,
+                    }}
+                    title="Mate's Bike"
+                    description="Your Friend Mate is riding his bike to work!"
+                    image={require('../../assets/bike.png')} // Replace with your local image path
+                />
+				<Marker
+                    coordinate={{
+                        latitude: 49.157,
+                        longitude: 9.216,
+                    }}
+                    title="Ashley Fletcher"
+                    description="Your Friend Ashely is walking to school today!"
+                    image={require('../../assets/profile_images/user1.png')} // Replace with your local image path
+                />
+				{/* <Grid rows={40} columns={35} latstart={49.125} lontstart={9.203}/> */}
                 {/* <GridPoligon x={49.151} y={9.215} opacity={1} /> */}
             </MapView>
         </View>
