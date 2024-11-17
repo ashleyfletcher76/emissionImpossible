a8331b694a806faa48d4aabd6b81552509549b52
Rodrigo Chávez <rodalcham@gmail.com>
2024-11-17 13:02:06 +0100

    latest version

--- app/screens/CityMap.js
+++ app/screens/CityMap.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Polygon } from 'react-native-maps';
 import { GridPoligon } from '../components/GridPoligon';
 import { Grid } from '../components/CityGrid';
 import { getCurrentPosition } from '../components/getCurrentPosition';
+import Icon from 'react-native-vector-icons/MaterialIcons'; // or FontAwesome, Ionicons, etc.
 
 export const CityMap = () => {
     const [location, setLocation] = useState({
@@ -12,6 +13,7 @@ export const CityMap = () => {
     });
     const [error, setError] = useState(null);
 
+	const [gridOn, setGridOn] = useState(false);
 	
     useEffect(() => {
 		const fetchLocation = async () => {
@@ -27,6 +29,12 @@ export const CityMap = () => {
 
     return (
         <View style={styles.container}>
+            <TouchableOpacity onPress={() => {
+                setGridOn(asd => !asd)}}
+                style={styles.iconButton}
+            >
+                <Icon name="grid-view" size={36} color="#000" />
+			</TouchableOpacity>
             {/* If there's an error, show it as a message */}
             {error && (
                 <Text style={styles.errorText}>{error.message || "Unknown error"}</Text> // Render error message if available
@@ -56,6 +64,7 @@ export const CityMap = () => {
                     title="Bus Line 42"
                     description="This icon follows bus line 42"
                     image={require('../../assets/bus.png')} // Replace with your local image path
+                    style={{ width: 30, height: 30 }}
                 />
 				<Marker
                     coordinate={{
@@ -65,6 +74,7 @@ export const CityMap = () => {
                     title="Mate's Bike"
                     description="Your Friend Mate is riding his bike to work!"
                     image={require('../../assets/bike.png')} // Replace with your local image path
+                    style={{ width: 30, height: 30 }}
                 />
 				<Marker
                     coordinate={{
@@ -74,8 +84,9 @@ export const CityMap = () => {
                     title="Ashley Fletcher"
                     description="Your Friend Ashely is walking to school today!"
                     image={require('../../assets/profile_images/user1.png')} // Replace with your local image path
+                    style={{ width: 20, height: 30 }}
                 />
-				{/* <Grid rows={40} columns={35} latstart={49.125} lontstart={9.203}/> */}
+				{gridOn && <Grid rows={40} columns={35} latstart={49.125} lontstart={9.203}/>}
                 {/* <GridPoligon x={49.151} y={9.215} opacity={1} /> */}
             </MapView>
         </View>
@@ -92,5 +103,13 @@ const styles = StyleSheet.create({
     errorText: {
         color: 'red', // Make the error message red
         padding: 10,
+    },
+    iconButton: {
+        backgroundColor: '#f0f0f0',
+        padding: 10,
+        borderRadius: 5,
+        zIndex: 5,
+        marginTop: 50,
+        position: "absolute"
     },
 });
